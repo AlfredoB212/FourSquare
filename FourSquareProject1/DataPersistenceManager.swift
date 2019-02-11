c6f7eb53160a86a04ba8fc9b9674cc53f65d0cfe
AlfredoB212 <alfredobarragan@Pursuits-MacBook-Pro-93.local>
2019-02-11 12:53:02 -0500

    added collection view constraints

---
+++ FourSquareProject1/DataPersistenceManager.swift
@@ -0,0 +1,20 @@
+//
+//  DataPersistenceManager.swift
+//  FourSquareProject1
+//
+//  Created by Alfredo Barragan on 2/11/19.
+//  Copyright © 2019 Alfredo Barragan. All rights reserved.
+//
+
+import Foundation
+final class DataPersistenceManager {
+    private init() {}
+    
+    static func documentsDirectory() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
+    static func filepathToDocumentsDirectory(filename: String) -> URL {
+        return documentsDirectory().appendingPathComponent(filename)
+    }
+}
