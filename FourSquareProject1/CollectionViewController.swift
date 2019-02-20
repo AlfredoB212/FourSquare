3930144e55a18ae094ffd837c1313a6d6f37b0fe
AlfredoB212 <alfredobarragan@Pursuits-MacBook-Pro-93.local>
2019-02-20 15:11:22 -0500

    configured cell and collectionview setup

--- FourSquareProject1/CollectionViewController.swift
+++ FourSquareProject1/CollectionViewController.swift
@@ -7,63 +7,9 @@
 //
 
 import UIKit
+class CollectionViewController: UIViewController {
 
-class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCellID", for: indexPath) as! collectionViewCell
-        return cell
-    }
-    
-    
-    let collectionViewCellID = "collectionViewCellID"
-    
-    lazy var myCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 16
-        layout.scrollDirection = .vertical
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .green
-        return cv
-    } ()
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(myCollectionView)
-        setupViews()
-        
     }
-    
-    
-    func setupViews() {
-        myCollectionView.delegate = self
-        myCollectionView.dataSource = self
-        
-        view.addSubview(myCollectionView)
-    
-        
-        myCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        myCollectionView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
-        myCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
-        myCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
-        myCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
-        
-    }
-    
-    class collectionViewCell: UICollectionViewCell {
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-    }
-
-
 }
